f1b287b85fa4ff14f203f8eec2e6c99b6b3ca45d
pangjianhai <821761509@qq.com>
2015-10-15 14:25:45 +0800

    init

---
+++ community_assist/src/cn/com/hzzc/industrial/pro/entity/ActivityEntity.java
@@ -0,0 +1,101 @@
+package cn.com.hzzc.industrial.pro.entity;
+
+/**
+ * @todo 活动entity
+ * @author pang
+ *
+ */
+public class ActivityEntity {
+
+	private String id;
+	private String title;
+	private String imgId;
+	private String introduction;
+	private String status;
+	private String readNum;
+	private String type;
+	private String createDate;
+	private String beginDate;
+	private String endDate;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getImgId() {
+		return imgId;
+	}
+
+	public void setImgId(String imgId) {
+		this.imgId = imgId;
+	}
+
+	public String getIntroduction() {
+		return introduction;
+	}
+
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getReadNum() {
+		return readNum;
+	}
+
+	public void setReadNum(String readNum) {
+		this.readNum = readNum;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getBeginDate() {
+		return beginDate;
+	}
+
+	public void setBeginDate(String beginDate) {
+		this.beginDate = beginDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+}
