46c2776f32f8140f1b460763d7d7315124cde503
pangjianhai <821761509@qq.com>
2015-10-20 14:56:58 +0800

    data

--- community_assist/src/cn/com/hzzc/industrial/pro/AddActivityActivity.java
+++ community_assist/src/cn/com/hzzc/industrial/pro/AddActivityActivity.java
@@ -19,12 +19,17 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.AdapterView.OnItemSelectedListener;
 import cn.com.hzzc.industrial.pro.cons.SystemConst;
 import cn.com.hzzc.industrial.pro.task.UploadFileTask;
+import cn.com.hzzc.industrial.pro.util.CommonDateUtil;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -36,8 +41,14 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 public class AddActivityActivity extends ParentActActivity implements
 		View.OnTouchListener, OnClickListener {
 
-	private EditText add_act_beginDate, add_act_endDate;
+	private EditText add_act_title, add_act_introduce, add_act_beginDate,
+			add_act_endDate;
 	private ImageView add_act_img, add_act_self;
+	private Spinner add_act_type;
+	private ArrayAdapter<String> typesAd = null;
+	private String type = "0";
+	String[] nt = { "线下活动", "调查活动", "统计活动" };
+	String file = "";
 	/**
 	 * 专属本社区
 	 */
@@ -49,9 +60,19 @@ public class AddActivityActivity extends ParentActActivity implements
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.add_act_intro);
 		init();
+		initTypeData();
+		initDate();
 	}
 
+	/**
+	 * @user:pang
+	 * @data:2015年10月20日
+	 * @todo:初始化元素
+	 * @return:void
+	 */
 	private void init() {
+		add_act_title = (EditText) findViewById(R.id.add_act_title);
+		add_act_introduce = (EditText) findViewById(R.id.add_act_introduce);
 		add_act_beginDate = (EditText) findViewById(R.id.add_act_beginDate);
 		add_act_endDate = (EditText) findViewById(R.id.add_act_endDate);
 		add_act_beginDate.setOnTouchListener(this);
@@ -62,6 +83,46 @@ public class AddActivityActivity extends ParentActActivity implements
 
 		add_act_self = (ImageView) findViewById(R.id.add_act_self);
 		add_act_self.setOnClickListener(this);
+
+		add_act_type = (Spinner) findViewById(R.id.add_act_type);
+	}
+
+	/**
+	 * @user:pang
+	 * @data:2015年10月20日
+	 * @todo:初始化选项
+	 * @return:void
+	 */
+	private void initTypeData() {
+		typesAd = new ArrayAdapter<String>(AddActivityActivity.this,
+				android.R.layout.simple_spinner_item, nt);
+		add_act_type.setAdapter(typesAd);
+		add_act_type.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				String proType = typesAd.getItem(arg2);
+				if ("线下活动".equals(proType)) {
+					type = "0";
+				} else if ("调查活动".equals(proType)) {
+					type = "1";
+				} else if ("统计活动".equals(proType)) {
+					type = "2";
+				}
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+
+			}
+		});
+	}
+
+	private void initDate() {
+		String b = CommonDateUtil.getNowTime();
+		b = b.substring(0, b.length() - 3);
+		add_act_beginDate.setText(b);
+		add_act_endDate.setText(b);
 	}
 
 	@Override
@@ -192,7 +253,9 @@ public class AddActivityActivity extends ParentActActivity implements
 	 * @todo:点击下一步
 	 * @return:void
 	 */
-	public void next(View v) {
+	@Override
+	public void sendSuccess() {
+		System.out.println("*******************************sendSuccess");
 		Intent intent = new Intent();
 		intent.setClass(AddActivityActivity.this,
 				ShowActivityDetailActivity.class);
@@ -200,18 +263,30 @@ public class AddActivityActivity extends ParentActActivity implements
 	}
 
 	@SuppressWarnings("unchecked")
-	public void saveShare(View view) {
-		String url = "";
+	public void next(View view) {
+		String url = SystemConst.server_url
+				+ SystemConst.Type2Url.save_act_type_2;
+		System.out.println("**************url:" + url);
+		String title = add_act_title.getText().toString();
+		String intro = add_act_introduce.getText().toString();
+		String begin = add_act_beginDate.getText().toString() + ":00";
+		String end = add_act_endDate.getText().toString() + ":00";
 		try {
 			Map map = new HashMap();
 
 			Map textPram = new HashMap();
 			List<File> files = new ArrayList<File>();
 			JSONObject obj = new JSONObject();
-			obj.put("userId", userId);
+			obj.put("type", type);
+			obj.put("title", title);
+			obj.put("introduction", intro);
+			obj.put("ifNeedSelfSociety", isSelf);
+			obj.put("beginDate", begin);
+			obj.put("endDate", end);
 			textPram.put(SystemConst.json_param_name, obj.toString());
-			for (int i = 0; i < selectedPicture.size(); i++) {
-				files.add(new File(selectedPicture.get(i)));
+			if (file != null && !"".equals(file)) {
+				System.out.println("*******file:" + file);
+				files.add(new File(file));
 			}
 			map.put(UploadFileTask.text_param, textPram);
 			map.put(UploadFileTask.file_param, files);
--- community_assist/src/cn/com/hzzc/industrial/pro/cons/SystemConst.java
+++ community_assist/src/cn/com/hzzc/industrial/pro/cons/SystemConst.java
@@ -12,4 +12,23 @@ public interface SystemConst {
 	 */
 	public static final String mobile_local_dir_for_pic = mobile_local_dir
 			+ "/PIC_COLLECTION";
+
+	public static final String server_url = "http://192.168.0.105:8080/IotApp/";
+
+	/**
+	 * 
+	 * @author pang
+	 * @todo URL常量
+	 *
+	 */
+	public class Type2Url {
+		/**
+		 * 保存类型2的活动
+		 */
+		public static final String save_act_type_2 = "commController/addCommenActivity.do";
+		public static final String questionItem = "commController/questionItem.do";
+		public static final String editQuestionItem = "commController/editQuestionItem.do";
+		public static final String delQuestionItem = "commController/delQuestionItem.do";
+		public static final String queryCommenActivityByIdForQuestionDetailIdByType = "commController/queryCommenActivityByIdForQuestionDetailIdByType.do";
+	}
 }
